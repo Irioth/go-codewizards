99ccbcdc3838db9c1c063e42df4c81afe79fe316
Irioth <irioth@gmail.com>
2016-11-14 19:23:27 +0300

    more methods on basic structs for flexebility

--- common.go
+++ common.go
@@ -17,6 +17,7 @@ const (
 	Faction_Other
 )
 
+// Unit -----------------------------------------------------------------------
 type Unit struct {
 	Id             int64
 	X, Y           float64
@@ -24,6 +25,15 @@ type Unit struct {
 	Angle          float64
 	Faction        Faction
 }
+
+func (u *Unit) GetId() int64        { return u.Id }
+func (u *Unit) GetX() float64       { return u.X }
+func (u *Unit) GetY() float64       { return u.Y }
+func (u *Unit) GetSpeedX() float64  { return u.SpeedX }
+func (u *Unit) GetSpeedY() float64  { return u.SpeedY }
+func (u *Unit) GetAngle() float64   { return u.Angle }
+func (u *Unit) GetFaction() Faction { return u.Faction }
+func (u *Unit) AsUnit() *Unit       { return u }
 
 func (u *Unit) GetAngleTo(x, y float64) float64 {
 	absolute_angle_to := math.Atan2(y-u.Y, x-u.X)
@@ -50,17 +60,41 @@ func (u *Unit) GetDistanceToUnit(unit *Unit) float64 {
 	return u.GetDistanceTo(unit.X, unit.Y)
 }
 
+type Point interface {
+	GetX() float64
+	GetY() float64
+}
+
+func (u *Unit) GetAngleToPoint(p Point) float64 {
+	return u.GetAngleTo(p.GetX(), p.GetY())
+}
+
+func (u *Unit) GetDistanceToPoint(p Point) float64 {
+	return u.GetDistanceTo(p.GetX(), p.GetY())
+}
+
+// CircularUnit ---------------------------------------------------------------
 type CircularUnit struct {
 	Unit
 	Radius float64
 }
 
+func (u *CircularUnit) GetRadius() float64            { return u.Radius }
+func (u *CircularUnit) AsCircularUnit() *CircularUnit { return u }
+
+// LivingUnit -----------------------------------------------------------------
 type LivingUnit struct {
 	CircularUnit
 	Life, MaxLife int
 	Statuses      []*Status
 }
 
+func (u *LivingUnit) GetLife() int              { return u.Life }
+func (u *LivingUnit) GetMaxLife() int           { return u.MaxLife }
+func (u *LivingUnit) GetStatuses() []*Status    { return u.Statuses }
+func (u *LivingUnit) AsLivingUnit() *LivingUnit { return u }
+
+// Statuses -------------------------------------------------------------------
 type StatusType int
 
 const (
